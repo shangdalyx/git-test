03361325629f397beaf12f19ec586181ac609d53
shangdalyx <90181436+shangdalyx@users.noreply.github.com>
2021-09-07 22:06:14 +0800

    Add files via upload

---
+++ GitTest.java
@@ -0,0 +1,12 @@
+package com.atliuyuxin.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git!");
+        System.out.println("hello git!!");
+        System.out.println("hello git!!!");
+        System.out.println("master test");
+        System.out.println("hot-fix test");
+    }
+}
